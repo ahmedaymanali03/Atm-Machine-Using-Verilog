d4616dfef47ad72229529b7973026dbed3f61f2f
Youssef Kassab <107130093+yakassab@users.noreply.github.com>
2023-11-26 17:18:58 +0200

    added check_Balance, update_balance, display_Balance,eject_Card states

--- MainModule.v
+++ MainModule.v
@@ -12,12 +12,13 @@ module ATM ();
     choose_Transaction = 4'b0010;
     deposit = 4'b0011; //salma
     withdraw= 4'b0100; //salma
-    check_Balance= 4'b0101; //ayman
-    update_balance= 4'b0110; //ayman
-    display_Balance= 4'b0111; //kassab
-    eject_Card= 4'b1000;//kassab
+    check_Balance= 4'b0101; //ayman DONE
+    update_balance= 4'b0110; //ayman DONE
+    display_Balance= 4'b0111; //kassab DONE
+    eject_Card= 4'b1000;//kassab DONE
     reg   [3:0]     current_state,
                      next_state;
+                     
 // state transition
      always @(posedge clk or negedge reset)
  begin
@@ -29,20 +30,46 @@ module ATM ();
    begin
      current_state <= next_state ;
    end
- end        
+ end 
+       
 // next_state logic
 always @(*)
- begin
-  case(current_state)
+begin
+case(current_state)
 	  
-Idle     						: begin
-								if(cardIn == 1'b1)
-									next_state = enter_Pin ;
-								else if (cardIn == 1'b0)
-									next_state = Idle ;	
-											  
-								end
-enter_Pin                      :be
-  endcase
- end
+Idle: begin
+				if(cardIn == 1'b1)
+					next_state = enter_Pin ;
+				else if (cardIn == 1'b0)
+					next_state = Idle ;	
+			end
+
+check_Balance: begin
+                if(inputAmount > balance)
+                  next_state = withdraw;
+                else
+                  next_state = update_balance;	
+               end
+               
+update_balance: begin
+                  next_state = display_Balance;
+                  if (moneyDeposited)
+                    balance = balance + inputAmount;
+                  else if (inputAmount)
+                    balance = balance - inputAmount;
+                end
+display_Balance:begin
+                    if (ejectCard)
+                    next_state = eject_Card;
+                    else
+                    next_state = choose_Transaction;
+                end
+eject_Card: begin
+              next_state = Idle;
+            end                
+
+
+
+endcase
+end
 endmodule
